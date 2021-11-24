18c99c2c5e701d33d1d394e68f1edb1fdfdf1cc9
fteychene <francois.teychene@gmail.com>
2021-11-24 22:49:58 +0100

    dependabot: Update versions
    feat: Add metrics

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.4.21"
+    kotlin("jvm") version "1.6.0"
     application
     id("com.github.johnrengelman.shadow") version "6.1.0"
     id("com.google.cloud.tools.jib") version "2.7.0"
@@ -18,11 +18,13 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation(platform("org.http4k:http4k-bom:3.280.0"))
+    implementation(platform("org.http4k:http4k-bom:4.17.1.0"))
     implementation("org.http4k:http4k-core")
     implementation("org.http4k:http4k-server-netty")
     implementation("org.http4k:http4k-format-jackson")
     implementation("org.http4k:http4k-client-websocket")
+    implementation("org.http4k:http4k-metrics-micrometer")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.7.2")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.11+")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
     implementation("io.github.microutils:kotlin-logging-jvm:2.0.2")
--- src/main/kotlin/fr/fteychene/teaching/cloud/failer/Main.kt
+++ src/main/kotlin/fr/fteychene/teaching/cloud/failer/Main.kt
@@ -1,5 +1,13 @@
 package fr.fteychene.teaching.cloud.failer
 
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics
+import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics
+import io.micrometer.core.instrument.binder.logging.LogbackMetrics
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics
+import io.micrometer.prometheus.PrometheusConfig
+import io.micrometer.prometheus.PrometheusMeterRegistry
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -9,6 +17,8 @@ import org.http4k.core.Status.Companion.INTERNAL_SERVER_ERROR
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.Status.Companion.PERMANENT_REDIRECT
 import org.http4k.filter.CachingFilters
+import org.http4k.filter.MicrometerMetrics
+import org.http4k.filter.ServerFilters
 import org.http4k.format.Jackson.auto
 import org.http4k.routing.ResourceLoader.Companion.Classpath
 import org.http4k.routing.ResourceLoader.Companion.Directory
@@ -41,26 +51,42 @@ fun Api(clock: Clock): HttpHandler {
     val healthLens = Body.auto<Health>().toLens()
     val errorLens = Body.auto<CustomError>().toLens()
 
-    return routes(
-        "/fail" bind Method.POST to {
-            GlobalScope.launch {
-                logger.warn("Received failure request. Shutting down in 3s")
-                delay(3000)
-                exitProcess(128)
+    val metrics = PrometheusMeterRegistry(PrometheusConfig.DEFAULT).apply {
+        config().commonTags("application", "cloud-failer")
+        config().commonTags("app-id", currentServerId.toString())
+        ClassLoaderMetrics().bindTo(this)
+        JvmMemoryMetrics().bindTo(this)
+        JvmGcMetrics().bindTo(this)
+        ProcessorMetrics().bindTo(this)
+        JvmThreadMetrics().bindTo(this)
+        LogbackMetrics().bindTo(this)
+    }
+
+    return ServerFilters.MicrometerMetrics.RequestCounter(metrics)
+        .then(ServerFilters.MicrometerMetrics.RequestTimer(metrics))
+        .then(routes(
+            "/fail" bind Method.POST to {
+                GlobalScope.launch {
+                    logger.warn("Received failure request. Shutting down in 3s")
+                    delay(3000)
+                    exitProcess(128)
+                }
+                Response(INTERNAL_SERVER_ERROR).with(errorLens of CustomError("Fail generated an error, server will shutdown in 3s"))
+            },
+            "/fail" bind Method.GET to {
+                Response(BAD_REQUEST).with(errorLens of CustomError("This route should be called with a POST"))
+            },
+            "/" bind Method.GET to {
+                Response(PERMANENT_REDIRECT).header("Location", "index.html")
+            },
+            "/" bind CachingFilters.Response.NoCache().then(static(Classpath("public"))),
+            "/health" bind Method.GET to {
+                Response(OK).with(healthLens of Health(Instant.now(clock)))
+            },
+            "/metrics" bind Method.GET to {
+                Response(OK).body(metrics.scrape())
             }
-            Response(INTERNAL_SERVER_ERROR).with(errorLens of CustomError("Fail generated an error, server will shutdown in 3s"))
-        },
-        "/fail" bind Method.GET to {
-            Response(BAD_REQUEST).with(errorLens of CustomError("This route should be called with a POST"))
-        },
-        "/" bind Method.GET to {
-            Response(PERMANENT_REDIRECT).header("Location", "index.html")
-        },
-        "/" bind CachingFilters.Response.NoCache().then(static(Classpath("public"))),
-        "/health" bind Method.GET to {
-            Response(OK).with(healthLens of Health(Instant.now(clock)))
-        }
-    )
+        ))
 }
 
 fun main() {
